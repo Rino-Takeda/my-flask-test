7b1a33a504fbc62af3dc28cf9c2fcb7d258be286
Rino-Takeda <r.takeda.24.7.1531@4nonome.com>
2025-07-19 01:07:55 +0900

    edit startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
 echo "Starting app with gunicorn..."
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
+exec gunicorn --bind=0.0.0.0 --timeout 600 app:app
